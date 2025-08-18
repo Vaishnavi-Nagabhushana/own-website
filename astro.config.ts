630c6c9ebe931aa559179c7cadcebf9b76ed26aa
Vaishnavi-Nagabhushana <vaishnavin027@gmail.com>
2025-08-18 22:11:25 +0530

    first commit

---
+++ astro.config.ts
@@ -0,0 +1,67 @@
+import { loadEnv } from "vite";
+import { defineConfig } from 'astro/config';
+
+import expressiveCode from 'astro-expressive-code';
+import mdx from '@astrojs/mdx';
+import sitemap from '@astrojs/sitemap';
+import spectre from './package/src';
+
+import node from '@astrojs/node';
+import { spectreDark } from './src/ec-theme';
+
+// const {
+//   GISCUS_REPO,
+//   GISCUS_REPO_ID,
+//   GISCUS_CATEGORY,
+//   GISCUS_CATEGORY_ID,
+//   GISCUS_MAPPING,
+//   GISCUS_STRICT,
+//   GISCUS_REACTIONS_ENABLED,
+//   GISCUS_EMIT_METADATA,
+//   GISCUS_LANG
+// } = loadEnv(process.env.NODE_ENV!, process.cwd(), "");
+
+// https://astro.build/config
+const config = defineConfig({
+  site: 'https://spectre.louisescher.dev',
+  output: 'static',
+  integrations: [
+    expressiveCode({
+      themes: [spectreDark],
+    }),
+    mdx(),
+    sitemap(),
+    spectre({
+      name: 'Home',
+      openGraph: {
+        home: {
+          title: 'Vaish\'s space',
+          description: 'A personal webpage'
+        },
+        blog: {
+          title: 'Blog',
+          description: 'Some interesting papers/topics on Continual Learning.'
+        },
+        projects: {
+          title: 'Projects'
+        }
+      }
+      // giscus: {
+      //   repository: GISCUS_REPO,
+      //   repositoryId: GISCUS_REPO_ID,
+      //   category: GISCUS_CATEGORY,
+      //   categoryId: GISCUS_CATEGORY_ID,
+      //   mapping: GISCUS_MAPPING as any,
+      //   strict: GISCUS_STRICT === "true",
+      //   reactionsEnabled: GISCUS_REACTIONS_ENABLED === "true",
+      //   emitMetadata: GISCUS_EMIT_METADATA === "true",
+      //   lang: GISCUS_LANG,
+      // }
+    })
+  ],
+  adapter: node({
+    mode: 'standalone'
+  })
+});
+
+export default config;
